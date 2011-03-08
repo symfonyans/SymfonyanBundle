a5335f23c3736c631b15562a947c20a40fcde1f4
Masao Maeda <brt.river@gmail.com>
2011-03-09 05:06:54 +0900

    first commit

---
+++ SymfonyanBundle/Command/SymfonyanExceptionInstallCommand.php
@@ -0,0 +1,64 @@
+<?php
+namespace Brtriver\SymfonyanBundle\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * SymfonyanExcepotionInstallCommand.
+ *
+ * @author brtriver <brt.river@gmail.com>
+ */
+class SymfonyanExceptionInstallCommand extends Command
+{
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    protected $container;
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->container = $this->application->getKernel()->getContainer();
+    }
+    /**
+     * @see Command
+     */
+    protected function configure()
+    {
+        $this
+            ->addOption('symlink', null, InputOption::VALUE_NONE, 'Symlinks the assets instead of copying it')
+            ->setName('symfonyan:exception-install')
+            ->setDescription('Change the icon of the exception page to Symfonyan')
+        ;
+    }
+
+    /**
+     * @see Command
+     *
+     * @throws \InvalidArgumentException When the target directory does not exist
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $filesystem = $this->container->get('filesystem');
+
+        // Create the bundles directory otherwise symlink will fail.
+        if (is_dir($originDir = __DIR__.'/../Resources/views/FrameworkBundle')) {
+            $output->writeln(sprintf('Installing symfonyan exception to <comment>app/FrameworkBundle</comment>'));
+
+            $targetDir = $this->container->getParameter('kernel.root_dir').'/FrameworkBundle';
+
+            $filesystem->remove($targetDir);
+
+            if ($input->getOption('symlink')) {
+                $filesystem->symlink($originDir, $targetDir);
+            } else {
+                $filesystem->mkdirs($targetDir, 0777);
+                $filesystem->mirror($originDir, $targetDir);
+            }
+        }
+    }
+}
---
+++ SymfonyanBundle/SymfonyanBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Brtriver\SymfonyanBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SymfonyanBundle extends Bundle
+{
+
+}
